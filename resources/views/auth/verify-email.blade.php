746951ab8fa5ad99b2dba0819272b95fbbcbf0d5
mabe4242 <kouiy42@gmail.com>
2025-07-23 15:20:00 +0900

    メール認証誘導画面のCSS調整

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,14 +1,21 @@
 @extends('layouts.app')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/auth/verify.css') }}">
+@endsection
+
 @section('content')
     <div class="verify-notice">
-        <p>登録していただいたメールアドレスに認証メールを送付しました。</p>
-        <p>メール認証を完了してください。</p>
-        <form method="GET" action="/mypage/profile">
-            <button type="submit">
-                認証はこちらから
-            </button>
-        </form>
-        <p><a href="">認証メールを再送する</a></p>
+        <div class="verify-notice__parts">
+            <p>登録していただいたメールアドレスに認証メールを送付しました。<br/>メール認証を完了してください。</p>
+            <form method="GET" action="/mypage/profile">
+                <button type="submit" class="verify-button">
+                    認証はこちらから
+                </button>
+            </form>
+            <div class="resend__link">
+                <a class="button-submit" href="">認証メールを再送する</a>
+            </div>
+        </div>
     </div>
 @endsection
