ea103358df47f999d60e0f25a6f342912bff71eb
mabe4242 <kouiy42@gmail.com>
2025-07-23 17:54:43 +0900

    会員登録画面のCSS調整

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,72 +6,53 @@
 
 @section('content')
 <div class="register-form__content">
-  <div class="register-form__heading">
-    <h2>会員登録</h2>
-  </div>
-  <form class="form" action="/register" method="post">
-    @csrf
-    <div class="form__group">
-      <div class="form__group-title">
-        <span class="form__label--item">お名前</span>
-      </div>
-      <div class="form__group-content">
-        <div class="form__input--text">
-          <input type="text" name="name" value="{{ old('name') }}" />
+    <form class="form" action="/register" method="post">
+        @csrf
+        <div class="register-form__heading">
+            <h2>会員登録</h2>
         </div>
-        <div class="form__error">
-          @error('name')
-          {{ $message }}
-          @enderror
+        <div class="form__group">
+            <label class="form__label--item" for="name">ユーザー名</label>
+            <input class="form__label--input" name="name" value="{{ old('name') }}" id="name"/>
+            <div class="form__error">
+                @error('name')
+                {{ $message }}
+                @enderror
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="form__group">
-      <div class="form__group-title">
-        <span class="form__label--item">メールアドレス</span>
-      </div>
-      <div class="form__group-content">
-        <div class="form__input--text">
-          <input name="email" value="{{ old('email') }}" />
+        <div class="form__group">
+            <label class="form__label--item" for="email">メールアドレス</label>
+            <input class="form__label--input" name="email" value="{{ old('email') }}" id="email"/>
+            <div class="form__error">
+                @error('email')
+                {{ $message }}
+                @enderror
+            </div>
         </div>
-        <div class="form__error">
-          @error('email')
-          {{ $message }}
-          @enderror
+        <div class="form__group">
+            <label class="form__label--item" for="password">パスワード</label>
+            <input class="form__label--input" name="password" value="{{ old('password') }}" type="password" id="password"/>
+            <div class="form__error">
+                @error('password')
+                {{ $message }}
+                @enderror
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="form__group">
-      <div class="form__group-title">
-        <span class="form__label--item">パスワード</span>
-      </div>
-      <div class="form__group-content">
-        <div class="form__input--text">
-          <input type="password" name="password" />
+        <div class="form__group">
+            <label class="form__label--item" for="password_confirmation">確認用パスワード</label>
+            <input class="form__label--input" name="password_confirmation" value="{{ old('password_confirmation') }}" type="password" id="password_confirmation"/>
+            <div class="form__error">
+                @error('password_confirmation')
+                {{ $message }}
+                @enderror
+            </div>
         </div>
-        <div class="form__error">
-          @error('password')
-          {{ $message }}
-          @enderror
+        <div class="form__button">
+            <button class="form__button-submit" type="submit">登録する</button>
         </div>
-      </div>
-    </div>
-    <div class="form__group">
-      <div class="form__group-title">
-        <span class="form__label--item">確認用パスワード</span>
-      </div>
-      <div class="form__group-content">
-        <div class="form__input--text">
-          <input type="password" name="password_confirmation" />
+        <div class="login__link">
+            <a class="login__button-submit" href="/login">ログインはこちら</a>
         </div>
-      </div>
-    </div>
-    <div class="form__button">
-      <button class="form__button-submit" type="submit">登録</button>
-    </div>
-  </form>
-  <div class="login__link">
-    <a class="login__button-submit" href="/login">ログインの方はこちら</a>
-  </div>
+    </form>
 </div>
 @endsection
