cc8a7121c427da9d83e55b93f370752ccbbb2091
mabe4242 <kouiy42@gmail.com>
2025-07-23 14:44:12 +0900

    プロフィール編集画面の作成

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -1,81 +1,57 @@
 @extends('layouts.app')
 
 @section('css')
-{{-- ここちゃんと後で css/profiles/edit.css のCSSに変えて！ --}}
-<link rel="stylesheet" href="{{ asset('css/auth/register.css') }}">
+<link rel="stylesheet" href="{{ asset('css/profiles/profile.css') }}">
 @endsection
 
 @section('content')
-<div class="register-form__content">
-  <div class="register-form__heading">
-    <h2>プロフィール設定</h2>
-  </div>
-  <form class="form" action="/mypage/profile" method="post">
-    @csrf
-    @method('PUT');
+<div class="profile__content">
+    <form class="profile__form" action="/mypage/profile" method="post" enctype="multipart/form-data">
+        @method('PUT')
+        @csrf
+        <h2 class="profile__heading">プロフィール設定</h2>
 
+        <div class="image-area">
+            {{-- Javascriptで画像表示の制御 --}}
+            <img src="/img/noimage.png" alt="プロフィール画像" class="image-parts">
+            <label class="image-label" for="image">画像を選択する</label>
+            <input class="image-select" type="file" name="photo" id="image">
+        </div>
 
-    {{-- ここに画像の設定項目 --}}
+        <div class="profile__parts">
+            <label class="profile__label" for="user_name">ユーザー名</label>
+            <input class="profile__input" type="text" name="user_name" id="user_name" >
+            @error('user_name')
+            <div class="error">{{ $message }}</div>
+            @enderror
+        </div>
 
+        <div class="profile__parts">
+            <label class="profile__label" for="postcode">郵便番号</label>
+            <input class="profile__input" type="text" name="postcode" id="postcode" >
+            @error('postcode')
+            <div class="error">{{ $message }}</div>
+            @enderror
+        </div>
 
-    <div class="form__group">
-      <div class="form__group-title">
-        <span class="form__label--item">ユーザー名</span>
-      </div>
-      <div class="form__group-content">
-        <div class="form__input--text">
-          <input type="text" name="name" value="{{ old('name') }}" />
+        <div class="profile__parts">
+            <label class="profile__label" for="address">住所</label>
+            <input class="profile__input" type="text" name="address" id="address" >
+            @error('address')
+            <div class="error">{{ $message }}</div>
+            @enderror
         </div>
-        <div class="form__error">
-          @error('name')
-          {{ $message }}
-          @enderror
+
+        <div class="profile__parts">
+            <label class="profile__label" for="building">建物名</label>
+            <input class="profile__input" type="text" name="building" id="building" >
+            @error('building')
+            <div class="error">{{ $message }}</div>
+            @enderror
         </div>
-      </div>
-    </div>
-    <div class="form__group">
-      <div class="form__group-title">
-        <span class="form__label--item">郵便番号</span>
-      </div>
-      <div class="form__group-content">
-        <div class="form__input--text">
-          <input type="email" name="email" value="{{ old('email') }}" />
-        </div>
-        <div class="form__error">
-          @error('email')
-          {{ $message }}
-          @enderror
-        </div>
-      </div>
-    </div>
-    <div class="form__group">
-      <div class="form__group-title">
-        <span class="form__label--item">住所</span>
-      </div>
-      <div class="form__group-content">
-        <div class="form__input--text">
-          <input type="password" name="password" />
-        </div>
-        <div class="form__error">
-          @error('password')
-          {{ $message }}
-          @enderror
-        </div>
-      </div>
-    </div>
-    <div class="form__group">
-      <div class="form__group-title">
-        <span class="form__label--item">建物名</span>
-      </div>
-      <div class="form__group-content">
-        <div class="form__input--text">
-          <input type="password" name="password_confirmation" />
-        </div>
-      </div>
-    </div>
-    <div class="form__button">
-      <button class="form__button-submit" type="submit">更新する</button>
-    </div>
-  </form>
+
+        <button class="submit-btn" type="submit">更新する</button>
+    </form>
 </div>
+
 @endsection
